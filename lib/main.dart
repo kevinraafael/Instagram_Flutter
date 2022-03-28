88a4aeb153f7dbf886a7979ccec60fcc19bab9d2
kevinraafael <kevinfrancelino@gmail.com>
2022-03-28 16:54:57 -0400

    feat: inicialização firebase web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,19 @@ import 'package:instagram_clone/utils/color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyA0RAd0fuz31prHbR81Ll7Su1KMNocmrc4',
+        appId: "1:613447041430:web:903ad6b3323e44447d5db7",
+        messagingSenderId: "613447041430",
+        projectId: "instagram-clone-9ecb4",
+        storageBucket: "instagram-clone-9ecb4.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
